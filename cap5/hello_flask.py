0a8bd0f95ad9a6e1c40cd8402f4bae1efe405d83
Jonathan Morais <jonathan_ms15@hotmail.com>
2024-11-09 10:07:39 -0300

    route /entry

--- cap5/hello_flask.py
+++ cap5/hello_flask.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import html
+from flask import Flask, render_template
 from vsearch import search4letters
 
 app = Flask(__name__)
@@ -12,6 +13,13 @@ def hello() -> str:
 @app.route("/search4")
 def do_search() -> str:
     return str(search4letters("life, the universe, and everything", "eiru!"))
+
+
+@app.route("/entry")
+def entry_page() -> "html":
+    return render_template(
+        "entry.html", the_title="Welcome to search4letters on the web!"
+    )
 
 
 app.run()
